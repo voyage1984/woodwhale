adcb3a1a94169de4504e53d94583d36e9184fcfa
efuious <519621oo>
2020-11-02 11:00:48 +0800

    修改登录逻辑

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-import Login
+
+from ui.mainwindow import Ui_MainWindow as MainWindow
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    login = Login.LoginWindow()
-    login.show()
+
+    mainwindow = MainWindow()
+    mainwindow.show()
+
     sys.exit(app.exec_())
