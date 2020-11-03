d723b9a75a46704ceda2152db7e0d55941ce6fdd
efuious <519621oo>
2020-11-03 09:31:13 +0800

    修改主事件

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-
-from ui.mainwindow import Ui_MainWindow as MainWindow
-
+from ui.home import Ui_home as Home
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    mainwindow = MainWindow()
-    mainwindow.show()
+    home = Home()
+    home.show()
 
     sys.exit(app.exec_())
