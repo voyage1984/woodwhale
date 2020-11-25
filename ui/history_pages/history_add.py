a292a3cd637a410cfebf69854be8d6142986c5f0
efuious <519621oo>
2020-11-25 11:20:00 +0800

    完善功能: 添加history

---
+++ ui/history_pages/history_add.py
@@ -0,0 +1,177 @@
+'''
+    * 添加 history_today  1
+    * 添加反馈（成功、失败）  1
+    * label输出文字对齐
+'''
+
+from PyQt5.QtWidgets import QLabel,QWidget,QHBoxLayout,QLineEdit,QTextEdit,QVBoxLayout,QPushButton,QMessageBox
+from PyQt5.QtCore import QThread,pyqtSignal
+
+import MyDatabase
+
+class history_add(QWidget):
+    def __init__(self):
+        self.db = MyDatabase.DBModel()
+        super().__init__()
+        self.init()
+
+    def set_db(self,db):
+        self.db = db
+
+    def init(self):
+        self.alert = alert_thread()
+        self.alert.message.connect(self.set_alert)
+
+        self.label_alert_status = QLabel("")
+        self.label_alert_detail = QLabel("")
+
+        label_date = QLabel('date: ')
+        label_title = QLabel('title: ')
+        label_article = QLabel('article: ')
+
+        label_date.setFixedSize(100,40)
+        label_title.setFixedSize(100,40)
+        label_article.setFixedSize(100,40)
+
+        self.label_alert_status.setFixedSize(100,50)
+        self.label_alert_detail.setFixedSize(400,50)
+        # self.label_alert_status.alignment()
+
+        self.input_date = QLineEdit()
+        self.input_title = QLineEdit()
+        self.input_article = QTextEdit()
+        self.submit_button = QPushButton('插入')
+        self.submit_button.clicked.connect(self.submit_event)
+
+        layout_date = QHBoxLayout()
+        layout_title = QHBoxLayout()
+        layout_article = QHBoxLayout()
+        layout_alert = QHBoxLayout()
+        
+        layout_date.addWidget(label_date)
+        layout_date.addWidget(self.input_date)
+        layout_title.addWidget(label_title)
+        layout_title.addWidget(self.input_title)
+        layout_article.addWidget(label_article)
+        layout_article.addWidget(self.input_article)
+        layout_alert.addWidget(self.label_alert_status)
+        layout_alert.addWidget(self.label_alert_detail)
+
+        self.layout = QVBoxLayout()
+        self.layout.addLayout(layout_date)
+        self.layout.addLayout(layout_title)
+        self.layout.addLayout(layout_article)
+        self.layout.addWidget(self.submit_button)
+        self.layout.addLayout(layout_alert)
+        self.setLayout(self.layout)
+
+    def submit_event(self):
+        if self.db.status(0) != False:
+            self.insert_data()
+        else:
+            print('连结数据库错误!: add_history.submit_event')
+
+    def insert_data(self):
+        print('开始插入数据: add_hesitory.insert_data')
+        date = self.input_date.text()
+        title = self.input_title.text()
+        article = self.input_article.toPlainText()
+        check = self.check_history(date,title,article)
+        if  check != False:
+            if check == 1:
+                print('错误: 存在空值')
+            elif check ==2:
+                print('错误: 日期已存在')
+                if self.alter_Event():
+                    self.db.update_history(date,title,article)
+                    self.clear_text()
+                    print('更新成功')
+                    self.add_alert(1)
+                    self.label_alert_detail.setText('更新成功！')
+            return
+        else:
+            if self.db.insert_history(date,title,article) == True:
+                print('添加成功！')
+                self.add_alert(1)
+                self.label_alert_detail.setText(date)
+                self.clear_text()
+            else:
+                self.add_alert(0)
+                print('添加失败！: add_history.insert_data')
+
+    def check_history(self,date,title,artile):
+        err_msg = "add_history.check_history"
+        result = 1
+        if len(date)==0 or len(title)==0 or len(artile)==0:
+            print('不能输入空值！: ',err_msg)
+            self.label_alert_detail.setText("存在空值！")
+        elif self.check_exist(date):
+            print('该日期已存在！')
+            self.label_alert_detail.setText("日期已存在！")
+            result = 2
+        else:
+            result = False
+        self.add_alert(0)
+        return result
+
+    def check_exist(self,date):
+        print('检查日期: ',date,"是否存在...")
+        search = self.db.get_search_from_table(0,date,'history')
+        if len(search) == 0:
+            print('未查询到数据: ',date)
+            return False
+        else:
+            print('已查询该数据: ',date)
+            return True
+
+
+    def clear_text(self):
+        self.input_date.setText("")
+        self.input_title.setText("")
+        self.input_article.setText("")
+
+    def add_alert(self,status):
+        self.alert.show_alert(status)
+
+    def set_alert(self,message):
+        print('设置状态: ',message)
+        self.label_alert_status.setText(message)
+
+    def alter_Event(self):
+        print('修改事件: history_add.alter_Event')
+        reply = QMessageBox.question(self, '内容已存在',
+                                     "是否更新?",QMessageBox.Yes, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            print('确认更新: history_add.alter_Event')
+            return True
+        else:
+            print('取消更新: history_add.alter_Event')
+            return False
+
+
+
+
+
+class alert_thread(QThread):
+    success = "成功！"
+    failed  = "失败！"
+
+    message = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.sec = 3000
+
+    def show_alert(self,status):
+        if status == 0:
+            self.message.emit(self.failed)
+        else:
+            self.message.emit(self.success)
+
+
+
+
+
+
+
