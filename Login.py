736309e17b857f4506bcf0c797f83450bafb9867
efuious <519621oo>
2020-11-25 11:22:49 +0800

    update

--- Login.py
+++ Login.py
@@ -24,7 +24,6 @@ class LoginWindow(QWidget):
 
     # 窗口设置
     def __init__gui(self):
-        print('创建登录框...')
         self.setWindowTitle("登录")
         self.setWindowIcon(QIcon('./src/logo.ico'))
         self.setInput()
@@ -36,11 +35,14 @@ class LoginWindow(QWidget):
 
     # 输入框
     def setInput(self):
-        print('创建输入框...')
         self.lab_username = QLabel('用户名：')
         self.lab_password = QLabel('密  码：')
         self.edit_username = QLineEdit()
         self.edit_password = QLineEdit()
+
+        self.edit_password.setText("password")
+        self.edit_username.setText("username")
+
         self.username = QHBoxLayout()
         self.password = QHBoxLayout()
         self.username.addWidget(self.lab_username)
@@ -53,17 +55,12 @@ class LoginWindow(QWidget):
 
     # 按钮框：确认 / 取消
     def setBtn(self):
-        print('创建按钮...')
         self.btn_login  = QPushButton('登录',self)
-        # self.btn_cancel = QPushButton('取消',self)
         self.btn_login.resize(self.btn_login.sizeHint())
-        # self.btn_cancel.resize(self.btn_cancel.sizeHint())
         self.btns = QHBoxLayout()
         self.btns.addWidget(self.btn_login)
-        # self.btns.addWidget(self.btn_cancel)
 
         self.btn_login.clicked.connect(self.confirmEvent)
-        # self.btn_cancel.clicked.connect(self.close)
 
     # 登录事件
     def confirmEvent(self):
@@ -74,7 +71,7 @@ class LoginWindow(QWidget):
            print('账号 / 密码不能为空！: Login.confirmEvent')
            self.nonEmpty()
         else:                           # 接入数据库
-            print('正在登录...')
+            print('正在登录: Login.LoginWindow.confirmEvent')
             try:
                 self.Login(name,password)
             except:
@@ -84,42 +81,37 @@ class LoginWindow(QWidget):
     def Login(self,name,password):
         self.db = MyDatabase.DBModel()
         if self.db.conn(name,password) == False:
-            print('登录错误！: Login.Login')
+            print('登录错误！: Login.LoginWindow.Login')
             self.loginError()
             return False
         else:
-            print('登录成功!')
+            print('登录成功!: Login.LoginWindow.Login')
             self._signal.emit(self.db)
             self.close()
 
 
     # 登录失败提示框
     def loginError(self):
+        print('登录失败!: Login.LoginWindow.loginError')
         QMessageBox.warning(self, '错误',
                             "用户名 / 密码错误！",
                             QMessageBox.Apply)
 
     # 判断输入是否为空
     def nonEmpty(self):
+        print('登录失败!: Login.LoginWindow.nonEmpty')
         QMessageBox.warning(self,'错误',
                                  "用户名 / 密码不能为空！",
                                  QMessageBox.Apply)
 
     # 取消事件
     def closeEvent(self, event):
-        print('取消事件: LogincloseEvent')
+        print('取消事件: Login.LoginWindow.LogincloseEvent')
         try:
-            if self.db.status() != False:
-                print('登录成功！: Login.closeEvent')
+            if self.db.status(0) != False:
+                print('登录成功！: Login.LoginWindow.Login.closeEvent')
             else:
-                print('登录失败！: Login.closeEvent')
+                print('登录失败！: Login.LoginWindow.Login.closeEvent')
         except:
-            print('登录失败！: Login.closeEvent')
-        # reply = QMessageBox.question(self, '警告',
-        #                              "确认退出?", QMessageBox.Yes |
-        #                              QMessageBox.No, QMessageBox.No)
-        #
-        # if reply == QMessageBox.Yes:
-        #     event.accept()
-        # else:
-        #     event.ignore()
+            print('登录失败！: Login.LoginWindow.Login.closeEvent')
+
