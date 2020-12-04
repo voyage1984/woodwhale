04a78cc22c220701d0f19ea24643106899044ab2
efuious <519621oo>
2020-12-04 08:54:26 +0800

    添加功能: 清空搜索记录

--- ui/history_pages/history_search_result.py
+++ ui/history_pages/history_search_result.py
@@ -7,26 +7,27 @@ class history_search_result(QWidget):
         self.init()
 
     def init(self):
-        self.label = QLabel("查询")
         self.list = QListWidget()
         self.list.addItem("-----搜索-----")
         self.layout = QGridLayout()
-        self.layout.addWidget(self.list)
+        self.layout.addWidget(self.list,0,0,10,8)
         self.setLayout(self.layout)
 
     def show_data(self,result):
         print("开始显示...")
         if len(result) == 0:
+            print('没有数据！')
             self.list.addItem("没有数据！")
             return
         for line in result:
             data = str(line[0]).strip()
             title = str(line[1]).strip()
             article = str(line[2]).strip()
-            string = data+title+article
+            string = data+'\t'+title+"\t"+article
             self.list.addItem(string)
             # self.list.addLayout(self.list_view(data,title,article))
-            QApplication.processEvents()
+        QApplication.processEvents()
+        print('显示完成')
 
     def list_view(self, data, title, article):
         label_data = QLabel(data)
@@ -38,3 +39,10 @@ class history_search_result(QWidget):
         line.addWidget(label_article)
 
         return line
+
+    def clear_data(self):
+        print('开始遍历: history_search_result.clear_data')
+        count = self.list.count()
+        print('遍历范围:',count)
+        for i in range(count):
+            self.list.takeItem(0)
