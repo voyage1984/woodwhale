c721f6b3b2a1d75d08e81b873228cb6da0bcda09
voyage1984 <voyage1984@gmail.com>
2021-03-19 17:35:55 +0800

    优化

--- ui/history_pages/history_search_result.py
+++ ui/history_pages/history_search_result.py
@@ -1,9 +1,10 @@
-from PyQt5.QtWidgets import QWidget,QHBoxLayout,QLabel,QListWidget,QGridLayout
+from PyQt5.QtWidgets import QWidget, QListWidget,QGridLayout
 from PyQt5.QtWidgets import QApplication
 
 import System
 
-from ui.history_pages import history_detail
+from ui import item_detail
+
 
 class history_search_result(QWidget):
     def __init__(self):
@@ -13,7 +14,7 @@ class history_search_result(QWidget):
 
     def set_db(self,db):
         self.db = db
-        self.detail.set_db(db)
+        self.detail.set_db(db,'history')
 
     def init(self):
         self.index = ''
@@ -25,7 +26,7 @@ class history_search_result(QWidget):
         self.layout.addWidget(self.list,0,0,10,8)
         self.setLayout(self.layout)
         self.list.itemClicked.connect(self.item_detail)
-        self.detail = history_detail.history_detail()
+        self.detail = item_detail.item_detail()
 
     def show_data(self,index,keyword,mode):
         self.index = index
@@ -43,69 +44,27 @@ class history_search_result(QWidget):
             print('没有数据！')
             self.list.addItem("没有数据！")
             return
-        for line in result:
-            print(line[1].encode('latin-1', errors='ignore').decode('gbk', errors='ignore'))
-            data = str(line[0]).strip()
-            title = str(line[1].encode('latin-1').decode('gbk')).strip()
-            article = str(line[2].encode('latin-1').decode('gbk')).strip()
-            string = data+'\t'+title+'\t'+article
-            self.list.addItem(string)
-            # self.list.addLayout(self.list_view(data,title,article))
+        System.set_list(self.list,result)
         QApplication.processEvents()
         print('显示完成')
 
-    def list_view(self, data, title, article):
-        label_data = QLabel(data)
-        label_title = QLabel(title)
-        label_article = QLabel(article)
-        line = QHBoxLayout()
-        line.addWidget(label_data)
-        line.addWidget(label_title)
-        line.addWidget(label_article)
-
-        return line
-
     def clear_data(self):
-        print('开始遍历: history_search_result.clear_data')
-        count = self.list.count()
-        print('遍历范围:',count)
-        for i in range(count):
-            self.list.takeItem(0)
-
-    def get_item(self,item):
-        result = item.text().split("\t")
-        return result
+        System.clear_data(self)
 
     def item_detail(self):
-        print('item detail')
+        print('item detail',System.func_name())
         item = self.list.currentItem()
-        date = self.get_item(item)[0]
-        if self.is_item(date) == False:
+        date = System.get_item(item)[0]
+        if System.is_item(date) == False:
             return
-        title = self.get_item(item)[1]
-        article = self.get_item(item)[2]
-        print(date)
-        self.detail._signal.connect(lambda:self.delete_item(item))
+        title = System.get_item(item)[1]
+        article = System.get_item(item)[2]
+        # print(date)
+        self.detail._signal.connect(self.renew_item)
         self.detail.set_content(date,title,article)
         self.detail.exec()
 
-    def delete_item(self,item):
-        # self.list.takeItem(self.list.row(item))
+    def renew_item(self):
         self.show_data(self.index,self.keyword,self.mode)
         print('已刷新')
 
-    def is_item(self,date):
-        print('解析结果：',date)
-        try:
-            result = date.replace('-','')
-            print('result = ',result)
-            if result.isdigit():
-                print('数据对象：',date)
-                return True
-            else:
-                print('非数据对象')
-                return False
-        except:
-            print('发生了错误！',System.func_name())
-            return False
-        return False
