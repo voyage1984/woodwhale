0be9739578cd153aa46fa6a67df62ef64ded54c2
voyage1984 <voyage1984@gmail.com>
2021-03-19 17:33:55 +0800

    增加功能

--- System.py
+++ System.py
@@ -17,6 +17,14 @@ def get_username(name):
     return result
 
 
+def clear_data(self):
+    print('开始遍历:',func_name())
+    count = self.list.count()
+    print('遍历范围:',count)
+    for i in range(count):
+        self.list.takeItem(0)
+
+
 def dialog(self,title,main):
     print('显示弹窗',func_name())
     reply = QMessageBox.question(self, title,
@@ -28,8 +36,45 @@ def dialog(self,title,main):
     else:
         return False
 
+def set_list(o_list,i_list):
+    print('开始设置list',func_name())
+    for line in i_list:
+        # print(line[1].encode('latin-1', errors='ignore').decode('gbk', errors='ignore'))
+        print(line[0])
+        data = str(line[0]).strip()
+        title = str(line[1].encode('latin-1').decode('gbk')).strip()
+        article = str(line[2].encode('latin-1').decode('gbk')).strip()
+        string = data + '\t' + title + '\t' + article
+        o_list.addItem(string)
+
+def get_item(item):
+    result = item.text().split("\t")
+    return result
+
+def is_item(date):
+    print('解析结果：',date)
+    try:
+        result = date.replace('-','')
+        result = result.replace(':','')
+        result = result.replace('.','')
+        result = result.replace(' ','')
+        print('result = ',result)
+        if result.isdigit():
+            print('数据对象：',date)
+            return True
+        else:
+            print('非数据对象')
+            return False
+    except:
+        print('发生了错误！',func_name())
+        return False
+
+
 
 if __name__ == '__main__':
     import MyDatabase
     db = MyDatabase.DBModel()
     db.conn(username,password)
+    list = db.get_all_from_table('recommend')
+    for i in list:
+        print(i[0])
