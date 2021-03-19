c4bb6f20dd9208454077382471be182f4e7e0e72
voyage1984 <voyage1984@gmail.com>
2021-03-19 17:36:24 +0800

    优化交互逻辑

--- ui/history_pages/history_add.py
+++ ui/history_pages/history_add.py
@@ -20,12 +20,6 @@ class history_add(QWidget):
         self.db = db
 
     def init(self):
-        self.alert = alert_thread()
-        self.alert.message.connect(self.set_alert)
-
-        self.label_alert_status = QLabel("")
-        self.label_alert_detail = QLabel("")
-
         label_date = QLabel('date: ')
         label_title = QLabel('title: ')
         label_article = QLabel('article: ')
@@ -34,9 +28,6 @@ class history_add(QWidget):
         label_title.setFixedSize(100,40)
         label_article.setFixedSize(100,40)
 
-        self.label_alert_status.setFixedSize(100,50)
-        self.label_alert_detail.setFixedSize(400,50)
-        # self.label_alert_status.alignment()
 
         self.input_date = QLineEdit()
         self.input_title = QLineEdit()
@@ -47,7 +38,6 @@ class history_add(QWidget):
         layout_date = QHBoxLayout()
         layout_title = QHBoxLayout()
         layout_article = QHBoxLayout()
-        layout_alert = QHBoxLayout()
         
         layout_date.addWidget(label_date)
         layout_date.addWidget(self.input_date)
@@ -55,15 +45,12 @@ class history_add(QWidget):
         layout_title.addWidget(self.input_title)
         layout_article.addWidget(label_article)
         layout_article.addWidget(self.input_article)
-        layout_alert.addWidget(self.label_alert_status)
-        layout_alert.addWidget(self.label_alert_detail)
 
         self.layout = QVBoxLayout()
         self.layout.addLayout(layout_date)
         self.layout.addLayout(layout_title)
         self.layout.addLayout(layout_article)
         self.layout.addWidget(self.submit_button)
-        self.layout.addLayout(layout_alert)
         self.setLayout(self.layout)
 
     def submit_event(self):
@@ -81,41 +68,33 @@ class history_add(QWidget):
         if  check != False:
             if check == 1:
                 print('错误: 存在空值')
+                System.dialog(self,'错误','存在空值')
+                return
             elif check ==2:
                 print('错误: 日期已存在')
                 if self.alter_Event():
-                    # self.db.update_history(date,title,article)
                     self.db.update_tdta("history",date,title,article)
                     self.clear_text()
                     print('更新成功')
-                    self.add_alert(1)
-                    self.label_alert_detail.setText('更新成功！')
             return
         else:
             if self.db.insert_tdta("history",date,title,article) == True:
             # if self.db.insert_history(date,title,article) == True:
                 print('添加成功！')
-                self.add_alert(1)
-                self.label_alert_detail.setText(date)
                 self.clear_text()
             else:
-                self.add_alert(0)
                 print('添加失败！',System.func_name())
                 self.show_err()
 
     def check_history(self,date,title,artile):
-        err_msg = "add_history.check_history"
         result = 1
         if len(date)==0 or len(title)==0 or len(artile)==0:
-            print('不能输入空值！: ',err_msg)
-            self.label_alert_detail.setText("存在空值！")
+            print('不能输入空值！:',System.func_name())
         elif self.check_exist(date):
-            print('该日期已存在！')
-            self.label_alert_detail.setText("日期已存在！")
+            print('该日期已存在！',System.func_name())
             result = 2
         else:
             result = False
-        self.add_alert(0)
         return result
 
     def check_exist(self,date):
@@ -134,13 +113,6 @@ class history_add(QWidget):
         self.input_title.setText("")
         self.input_article.setText("")
 
-    def add_alert(self,status):
-        self.alert.show_alert(status)
-
-    def set_alert(self,message):
-        print('设置状态: ',message)
-        self.label_alert_status.setText(message)
-
     def alter_Event(self):
         print('修改事件: history_add.alter_Event')
         if System.dialog(self,'内容已存在',"是否更新?",):
@@ -152,23 +124,6 @@ class history_add(QWidget):
 
     def show_err(self):
         System.dialog(self,'插入失败！',"请检查输入")
-
-class alert_thread(QThread):
-    success = "成功！"
-    failed  = "失败！"
-
-    message = pyqtSignal(str)
-
-    def __init__(self):
-        super().__init__()
-        self.sec = 3000
-
-    def show_alert(self,status):
-        if status == 0:
-            self.message.emit(self.failed)
-        else:
-            self.message.emit(self.success)
-
 
 
 
