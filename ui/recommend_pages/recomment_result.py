b28cca0858aeb6e346d2c95a9212c0c14de57e77
voyage1984 <voyage1984@gmail.com>
2021-03-19 17:34:28 +0800

    增加功能：显示detail

--- ui/recommend_pages/recomment_result.py
+++ ui/recommend_pages/recomment_result.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QWidget,QHBoxLayout,QLabel,QListWidget,QGridLayout
+from PyQt5.QtWidgets import QWidget, QListWidget,QGridLayout
 from PyQt5.QtWidgets import QApplication
 
 import System
+from ui import item_detail
+
 
 class recommend_result(QWidget):
     def __init__(self):
@@ -11,15 +13,20 @@ class recommend_result(QWidget):
 
     def set_db(self,db):
         self.db = db
+        self.detail.set_db(db,'recommend')
 
     def init(self):
+        self.keyword = ''
         self.list = QListWidget()
         self.list.addItem("-----结果-----")
         self.layout = QGridLayout()
         self.layout.addWidget(self.list,0,0,10,8)
         self.setLayout(self.layout)
+        self.list.itemClicked.connect(self.item_detail)
+        self.detail = item_detail.item_detail()
 
     def get_data(self):
+        System.clear_data(self)
         if self.db == None:
             print('数据库初始化失败！',System.func_name())
             return
@@ -28,13 +35,34 @@ class recommend_result(QWidget):
             print('没有数据',System.func_name())
             self.list.addItem('没有数据！')
             return
-        for line in list:
-            # print(line[1].encode('latin-1', errors='ignore').decode('gbk', errors='ignore'))
-            data = str(line[0]).strip()
-            title = str(line[1].encode('latin-1').decode('gbk')).strip()
-            article = str(line[2].encode('latin-1').decode('gbk')).strip()
-            string = data + '\t' + title + '\t' + article
-            self.list.addItem(string)
+        System.set_list(self.list,list)
         QApplication.processEvents()
         print('显示完成')
+
+    def set_data(self,keyword):
+        self.keyword = keyword
+        print('开始获取内容',System.func_name())
+        System.clear_data(self)
+        r_title = self.db.get_search_from_table(1,keyword,'recommend',0)
+        System.set_list(self.list,r_title)
+        QApplication.processEvents()
+        print('显示完成')
+
+    def item_detail(self):
+        print('item detail',System.func_name())
+        item = self.list.currentItem()
+        date = System.get_item(item)[0][:-3]
+        if System.is_item(date) == False:
+            print('非item')
+        else:
+            title = System.get_item(item)[1]
+            article = System.get_item(item)[2]
+            print(date+'\t'+title+'\t'+article)
+            self.detail._signal.connect(self.renew_item)
+            self.detail.set_content(date, title, article)
+            self.detail.exec()
+
+    def renew_item(self):
+        self.set_data(self.keyword)
+
 
