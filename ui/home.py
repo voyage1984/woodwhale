689b231f0b1ef5f6089e94a2834e80567780735c
voyage1984 <voyage1984@gmail.com>
2021-04-03 23:00:40 +0800

    完善功能：图书管理

--- ui/home.py
+++ ui/home.py
@@ -13,8 +13,11 @@ from PyQt5.QtGui import QIcon,QPixmap
 from Login import LoginWindow as loginWindow
 from ui.tools import tools
 from ui.personal import personal as page_index
+from ui.book_manage import book_manage
 import MyDatabase
 import System
+
+from ui.personal import personal as user_manage
 
 class Ui_home(QWidget):
     def __init__(self):
@@ -29,28 +32,30 @@ class Ui_home(QWidget):
         self.click_event()
 
     def mainwindow(self):
-        self.setWindowIcon(QIcon('./src/logo.ico'))
+        self.setWindowIcon(QIcon('./src/whale.ico'))
         self.setFixedSize(1280, 720)
         self.setWindowTitle('木鲸图书馆管理系统')
         self.layout = QHBoxLayout()
-        # self.setStyleSheet('background-color:lightblue')
 
     def rightwindow(self):
         self.rightFrame = QFrame(self)
-        # self.rightFrame.setStyleSheet('background-color:lightyellow')
         
-        self.page1 = loginWindow()
-        self.page2 = tools()
-        self.page1_index = page_index()
+        self.p_login = loginWindow()
+        self.p_tools= tools()
+        self.p_book_manage = book_manage()
+        self.user_manage = user_manage()
+        self.p_index = page_index()
 
         self.qsl = QStackedLayout(self.rightFrame)
-        self.qsl.addWidget(self.page1)
-        self.qsl.addWidget(self.page2)
-        self.qsl.addWidget(self.page1_index)
+        self.qsl.addWidget(self.p_login)
+        self.qsl.addWidget(self.p_index)
+        self.qsl.addWidget(self.p_tools)
+        self.qsl.addWidget(self.p_book_manage)
+
 
     def leftwindow(self):
         self.label = QLabel(self)
-        self.label.setPixmap(QPixmap('./src/logo.jpg'))
+        self.label.setPixmap(QPixmap('./src/whale.ico'))
         self.label.setFixedSize(200, 200)
 
         self.btn_login = QPushButton('登录')
@@ -70,23 +75,24 @@ class Ui_home(QWidget):
         self.setLayout(self.layout)
 
     def click_event(self):
-        self.page1._signal.connect(self.get_db)
-        self.page1_index._signal.connect(self.logout)
+        self.p_login._signal.connect(self.get_db)
+        self.p_index._signal.connect(self.logout)
         self.btn_login.clicked.connect(lambda :self.to_page(0))
         self.btn_tools.clicked.connect(lambda :self.to_page(1))
+        self.btn_book_management.clicked.connect(lambda :self.to_page(2))
 
 
     def to_page(self,num):
         status = self.test_db()
         if num == 0:
             if status:
-                self.qsl.setCurrentIndex(2)
+                self.qsl.setCurrentIndex(1)
             else:
                 self.qsl.setCurrentIndex(0)
                 self.btn_login.setText('登录')
-        elif num == 1:
+        elif num >= 1:
             if status:
-                self.qsl.setCurrentIndex(1)
+                self.qsl.setCurrentIndex(1+num)
             else:
                 self.login_Error()
 
@@ -94,19 +100,20 @@ class Ui_home(QWidget):
         print('已登录')
         self.db = db
         self.btn_login.setText('个人信息')
-        self.qsl.setCurrentIndex(2)
+        self.qsl.setCurrentIndex(1)
         self.set_child_db()
 
     def set_child_db(self):
-        self.page1_index.setdb(self.db)
-        self.page2.setdb(self.db)
+        print("更新数据库对象...")
+        self.p_index.setdb(self.db)
+        self.p_tools.setdb(self.db)
+        self.p_book_manage.setdb(self.db)
 
     def logout(self):
         print('登出: home.logout')
         self.db = None
         self.btn_login.setText('登录')
         self.qsl.setCurrentIndex(0)
-        # self.set_child_db()
 
     def test_db(self):
         try:
