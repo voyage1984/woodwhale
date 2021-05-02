c554d41495bf12a8a94167504ac57717600ba52f
voyage1984 <voyage1984@gmail.com>
2021-05-02 22:51:20 +0800

    创建list模型

---
+++ ui/model/listmodel.py
@@ -0,0 +1,39 @@
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QWidget, QVBoxLayout
+
+
+class listmodel(QWidget):
+    def __init__(self,num,title,author):
+        super().__init__()
+        self.init(num,title,author)
+
+    def init(self,num,title,author):
+        label_id = QLabel(num)
+        label_title = QLabel(title)
+
+        label_author = QLabel(author)
+        label_id.setFixedSize(50,30)
+        label_title.setFixedSize(200,30)
+        label_author.setFixedSize(150,30)
+
+        label_id.setFont(QFont("Microsoft YaHei",10,QFont.Black))
+        label_title.setFont(QFont("Microsoft YaHei",10,QFont.Black))
+        label_author.setFont(QFont("Microsoft YaHei",10,QFont.Black))
+
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(label_id)
+        hlayout.addWidget(label_title)
+        hlayout.addWidget(label_author)
+        hlayout.setAlignment(Qt.AlignTop)
+
+        label_line = QLabel("")
+        label_line.setFixedSize(400,1)
+        label_line.setStyleSheet("background:transparent;border:2px solid black;")
+
+        layout = QVBoxLayout()
+        layout.addLayout(hlayout)
+        layout.addWidget(label_line)
+
+
+        self.setLayout(layout)
